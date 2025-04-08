0a7fbb24467f05c51388ff730d31cda115cb9ebd
KrDw <mail@k1r.in>
2025-04-08 13:57:35 +0800

    fix: 删除脚本名称中的后缀

--- client/restic-backup-ntfy.sh
+++ client/restic-backup-ntfy.sh
@@ -72,7 +72,7 @@ restic_check() {
 # ===== 变量声明 =====
 # 定义脚本根目录
 BASE_DIR="$(dirname "$0")/.."
-SCRIPT_NAME="$(basename "$0")"
+SCRIPT_NAME="$(basename "$0" .sh)"
 
 LOG_DIR="$BASE_DIR/logs"
 LOG_FILE="$LOG_DIR/restic-backup-$(date '+%Y%m').log"
